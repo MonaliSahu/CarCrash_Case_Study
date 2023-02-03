35d85b2676e9096fa801bc755d16ae093cfa1e04
MonaliSahu <48520317+MonaliSahu@users.noreply.github.com>
2023-02-03 15:22:45 +0530

    Add files via upload

---
+++ execution.bat
@@ -0,0 +1,16 @@
+@ECHO OFF 
+
+pip install pyspark
+pip install pathlib
+
+tar -xf Data.zip
+
+ECHO "Data.zip unzipped successfully"
+
+cd Source_code
+
+ECHO "Inside Source_code"
+
+spark-submit --master local[*] main_module.py "config.json"
+
+cmd /k execution.bat
